b1f01c1cedd9a88b5c586f02f4f25690eb5f62e0
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2025-03-05 19:21:19 -0600

    Add project files.

---
+++ SC-601-PA-G5-M/Models/Taller/CitaTaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SC_601_PA_G5_M.Models.Taller
+{
+    public class CitaTaller
+    {
+        [Key]
+        public int IdCita { get; set; }
+
+        [ForeignKey("Usuario")]
+
+        public int UsuarioId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "Fecha de la cita")]
+        public DateTime FechaCita { get; set; }
+
+        [Required(ErrorMessage = "La descripcion del servicio es obligatorio")]
+        [StringLength(100)]
+        public string DescripcionServicio { get; set; }
+
+        [Required(ErrorMessage = "El estado de la cita es obligatorio")]
+        [StringLength(50)]
+        public string EstadoCita { get; set; }
+    }
+}
---
+++ SC-601-PA-G5-M/Models/Taller/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SC_601_PA_G5_M.Models.Taller
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [StringLength(50)]
+        public string CorreoElectronico { get; set; }
+
+        [Required(ErrorMessage = "El telefono es obligatorio")]
+        [StringLength(50)]
+        public string Telefono { get; set; }
+    }
+}
